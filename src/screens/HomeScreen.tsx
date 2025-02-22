5358c993e7c0029d40689eac2941c385ae12095c
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-02-22 18:23:48 +0530

    carouseel added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -36,10 +36,14 @@ const HomeScreen = () => {
 
   const handleAddToCart = (product) => {
     setCart((prevCart) => {
-      const updatedCart = prevCart.map((item) =>
-        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-      );
-      return existingItem ? updatedCart : [...prevCart, { ...product, quantity: 1 }];
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
     });
   };
   const handleIncreaseQuantity = (productId) => {
@@ -134,6 +138,8 @@ const HomeScreen = () => {
     </View>
   </Modal>
 )}
+
+
 
 
 
